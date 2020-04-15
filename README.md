7de0551f4f366ffe26cbc22a7bde000fbd3816ff
Daniel Schiffner <schiffner@dipf.de>
2020-04-15 17:17:08 +0200

    Updating features - now supports dry-run - now supports reading json configs - better readme

--- README.md
+++ README.md
@@ -11,6 +11,8 @@ Note: __Requires python 3__
 | source | The original git-url that was being forked from and was updated (should be synced) |
 | fork | The forked repository url |
 | namespace | a namespace (i.e. a folder) that is created in the fork (default: `source`) |
+| --dry-run | doing a dry run |
+| --file / -f | Loading a json to apply multiple repositories at once |
 
 All branches and tags from the source are transferred to the fork and are stored in the folder given in `namespace`
 * branches/tags will be generated using a folder structure using the namespace
@@ -18,7 +20,83 @@ All branches and tags from the source are transferred to the fork and are stored
 ## Configuration / URLs
 To allow cloning and pushing to gitlab, a deploy key is required. For just cloning, a deploy token is sufficient.
 
-The tool uses an `id_rsa`-file in the working directory where it was called (if present).
+The tool uses an `id_{rsa,ed25519,whatever}`-file in the working directory where it was called (if present).
 
 Otherwise, provide a full url with credentials for cloning:
 `https://gitlab+deploy-token-12:t67NCHtu3%fINVALDIG!!!FNOTWORKING"!!@gitlab.tba-hosting.de/MySkills/bke-sourceclone/tao-php-docker.git`
+
+### JSON
+Schema:
+```json
+{
+    "$schema": "http://json-schema.org/draft-07/schema",
+    "$id": "http://gitlab.tba-hosting.de/MySkills/Git-Sync.json",
+    "type": "array",
+    "title": "Git Sync Schema",
+    "description": "A schema for specifing repos used for the Git-Sync tool.",
+    "default": [],
+    "additionalItems": true,
+    "items": {
+        "$id": "#/items",
+        "type": "object",
+        "title": "An configuration for syncing repositories",
+        "default": {},
+        "examples": [
+            {
+                "fork": "ssh://git@gitlab.tba-hosting.de:47478/MySkills/bke-sourceclone/TAO.git",
+                "source": "https://gitlab.opit.hu/BKE/TAO.git",
+                "id_rsa": "gitlab",
+                "namespace": "tba21"
+            }
+        ],
+        "additionalProperties": true,
+        "required": [
+            "source",
+            "fork",
+            "namespace"
+        ],
+        "properties": {
+            "source": {
+                "$id": "#/items/properties/source",
+                "type": "string",
+                "title": "Source Repository",
+                "description": "Access URL for the original repository.",
+                "default": "",
+                "examples": [
+                    "https://gitlab.opit.hu/BKE/TAO.git"
+                ]
+            },
+            "fork": {
+                "$id": "#/items/properties/fork",
+                "type": "string",
+                "title": "Forked Repository",
+                "description": "Access URL for the forked repository.",
+                "default": "",
+                "examples": [
+                    "ssh://git@gitlab.tba-hosting.de:47478/MySkills/bke-sourceclone/TAO.git"
+                ]
+            },
+            "namespace": {
+                "$id": "#/items/properties/namespace",
+                "type": "string",
+                "title": "Namespace",
+                "description": "A generic prefix used for creating branches or tags.",
+                "default": "",
+                "examples": [
+                    "tba21"
+                ]
+            },
+            "ssh_file": {
+                "$id": "#/items/properties/id_rsa",
+                "type": "string",
+                "title": "Private ssh_file file",
+                "description": "An path to an private ssh id file",
+                "default": "",
+                "examples": [
+                    "id_rsa"
+                ]
+            }
+        }
+    }
+}
+```
--- sync.py
+++ sync.py
@@ -6,56 +6,93 @@ import os
 import tempfile
 
 args = ArgumentParser()
-args.add_argument("source")
-args.add_argument("fork")
+args.add_argument("-f", "--file", nargs="?")
+args.add_argument("--dry-run", action="store_true")
+args.add_argument("source", nargs="?")
+args.add_argument("fork", nargs="?")
 args.add_argument("namespace", default="source", nargs="?")
 opts = args.parse_args()
 
 env = {}
 
 def git(*args):
-    global env
+    global env, opts
+    if opts.dry_run:
+        print(f"Would call 'git {' '.join(args)}'  ", end="")
+        res = type("Dummy", (object,), {})()
+        res.stdout = b""
+        return res
     return run(['git'] + list(args), check=True, env=env, capture_output=True)
 
-if(os.path.exists("id_rsa")):
-    env["GIT_SSH_COMMAND"] = f'ssh -i {os.getcwd()}/id_rsa -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no'
 
-
-skipped = 0
-with tempfile.TemporaryDirectory() as tmpdir:
-    os.chdir(tmpdir)
-    print(f"Working in {os.getcwd()}")
-    try:
-        print("Cloning fork ", end="")
-        git("clone", "--bare", opts.fork, ".")
-        print("✓")
-        print("Fetching source ", end="")
-        git("remote", "add", "upstream", opts.source)
-        git('fetch', 'upstream')
-        print("✓")
-        print("Copy branches", end="")
-        git("push", "-f", "origin", f"refs/remotes/upstream/*:refs/heads/{opts.namespace}/*")
-        print("✓")
-        tags = git("tag", "-l").stdout.decode().split()
-        for t in tags:
-            #avoid nesting (shouldnt happen)
-            #and additional pushing of tags if they are already present
-            if(t.startswith(opts.namespace)):
-                skipped += 1
-                #print(f"Skipping already namespaced tag {t}")
-                continue
-            if(f"{opts.namespace}/{t}" in tags):
-                #print(f"Skipping {t} as it is already present")
-                skipped += 1
-                continue
-            print(f"Pushing tag {t}", end="")
-            git("push", "-f", "origin", f"refs/tags/{t}:refs/tags/{opts.namespace}/{t}")
+def mirror(source, fork, namespace):
+    skipped = 0
+    with tempfile.TemporaryDirectory() as tmpdir:
+        os.chdir(tmpdir)
+        print(f"Working in {os.getcwd()}")
+        try:
+            print("Cloning fork ", end="")
+            git("clone", "--bare", fork, ".")
             print("✓")
-    except Exception as e:
-        print("Failed to invoke command", e)
-        print(e.stdout)
-        print(e.stderr)
-        print(e.args)
-    print("Cleanup ", end="")
-print("✓")
-print(f"Skipped {skipped} tags")
+            print("Fetching source ", end="")
+            git("remote", "add", "upstream", source)
+            git('fetch', 'upstream')
+            print("✓")
+            print("Copy branches", end="")
+            git("push", "-f", "origin", f"refs/remotes/upstream/*:refs/heads/{namespace}/*")
+            print("✓")
+            tags = git("tag", "-l").stdout.decode().split()
+            for t in tags:
+                #avoid nesting (shouldnt happen)
+                #and additional pushing of tags if they are already present
+                if(t.startswith(namespace)):
+                    skipped += 1
+                    #print(f"Skipping already namespaced tag {t}")
+                    continue
+                if(f"{namespace}/{t}" in tags):
+                    #print(f"Skipping {t} as it is already present")
+                    skipped += 1
+                    continue
+                print(f"Pushing tag {t}", end="")
+                git("push", "-f", "origin", f"refs/tags/{t}:refs/tags/{namespace}/{t}")
+                print("✓")
+        except Exception as e:
+            print("Failed to invoke command", e)
+            print(e.stdout)
+            print(e.stderr)
+            print(e.args)
+        finally:
+            print("Cleanup ", end="")
+    print("✓")
+    print(f"Skipped {skipped} tags")
+
+def set_id_rsa(path):
+    if(os.path.exists(os.path.join(os.path.abspath(path), "id_rsa"))):
+        env["GIT_SSH_COMMAND"] = f'ssh -i {path}/id_rsa -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no'
+
+def main():
+    global env
+    set_id_rsa(os.getcwd())
+    env["BASE_GIT_SSH_COMMAND"] = env["GIT_SSH_COMMAND"]
+    skipped = 0
+    if opts.file != None and os.path.exists(opts.file):
+        import json
+        with open(opts.file) as fp:
+            repos = json.load(fp)
+        for repo in repos:
+            if "ssh_file" in repo:
+                print("Using own SSH Key", repo)
+                set_id_rsa(repo['ssh_file'])
+            mirror(repo["source"], repo["fork"], repo["namespace"])
+            #restore
+            if "ssh_file" in repo:
+                env["GIT_SSH_COMMAND"] = env["BASE_GIT_SSH_COMMAND"]
+
+    else:
+        if not opts.source or not opts.fork:
+            args.print_usage()
+            exit(1)
+        mirror(opts.source, opts.fork, opts.namespace)
+
+if __name__ == "__main__":
+    main()
