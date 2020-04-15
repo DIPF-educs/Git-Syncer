78ac79fa8a52462fbfd748adce55fe9db98293c4
Daniel Schiffner <schiffner@dipf.de>
2020-04-15 14:28:11 +0200

    Updating readme Adding tags Optimizing prefixing branches /close 1

--- sync.py
+++ sync.py
@@ -21,10 +21,10 @@ if(os.path.exists("id_rsa")):
     env["GIT_SSH_COMMAND"] = f'ssh -i {os.getcwd()}/id_rsa -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no'
 
 
+skipped = 0
 with tempfile.TemporaryDirectory() as tmpdir:
     os.chdir(tmpdir)
-    print(f"Currently in {os.getcwd()}")
-
+    print(f"Working in {os.getcwd()}")
     try:
         print("Cloning fork ", end="")
         git("clone", "--bare", opts.fork, ".")
@@ -33,18 +33,29 @@ with tempfile.TemporaryDirectory() as tmpdir:
         git("remote", "add", "upstream", opts.source)
         git('fetch', 'upstream')
         print("✓")
-        branches = git("branch", "-a")
-        for br in branches.stdout.decode().split():
-            if(not br.startswith('remotes/upstream')):
+        print("Copy branches", end="")
+        git("push", "-f", "origin", f"refs/remotes/upstream/*:refs/heads/{opts.namespace}/*")
+        print("✓")
+        tags = git("tag", "-l").stdout.decode().split()
+        for t in tags:
+            #avoid nesting (shouldnt happen)
+            #and additional pushing of tags if they are already present
+            if(t.startswith(opts.namespace)):
+                skipped += 1
+                #print(f"Skipping already namespaced tag {t}")
                 continue
-            br = br[len("remotes/upstream/"):] 
-            print(f"Pushing branch {br}", end="")
-            git("push", "-f", "origin", f"refs/remotes/upstream/{br}:refs/heads/{opts.namespace}/{br}")
+            if(f"{opts.namespace}/{t}" in tags):
+                #print(f"Skipping {t} as it is already present")
+                skipped += 1
+                continue
+            print(f"Pushing tag {t}", end="")
+            git("push", "-f", "origin", f"refs/tags/{t}:refs/tags/{opts.namespace}/{t}")
             print("✓")
     except Exception as e:
         print("Failed to invoke command", e)
         print(e.stdout)
         print(e.stderr)
         print(e.args)
-    print("Would Cleanup ", end="")
+    print("Cleanup ", end="")
 print("✓")
+print(f"Skipped {skipped} tags")
